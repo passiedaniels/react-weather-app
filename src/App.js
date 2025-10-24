0276cd98657434f644aca44b1996c1fc0ad435fe
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-10-25 01:33:47 +0300

    added links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import React from "react";
 import "./App.css";
-import Weather from "./Weather";
 
 function App() {
   return (
@@ -9,4 +9,28 @@ function App() {
   );
 }
 
-export default App;
+export default function Weather() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/passiedaniels" target="_blank">
+          Pascalia A.
+        </a>
+        , open sourced on{" "}
+        <a
+          href="https://github.com/passiedaniels/react-weather-app"
+          target="_blank"
+        >
+          github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://myreactweathe.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </p>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,2 @@
-import React from "react";
 
-export default function Weather() {
-  return <div>Weather App</div>;
-}
+
