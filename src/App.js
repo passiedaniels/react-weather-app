d57974103749aff7fa2b402dcb3c09cccb5e58b2
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-10-26 00:46:46 +0300

    added bootsrap and html

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div>
       <Weather />
-    </div>
-  );
-}
-
-export default function Weather() {
-  return (
-    <div>
-      <h1>Weather App</h1>
-
-      <p>
-        This project was coded by{" "}
-        <a href="https://github.com/passiedaniels" target="_blank">
-          Pascalia A.
-        </a>
-        , open sourced on{" "}
-        <a
-          href="https://github.com/passiedaniels/react-weather-app"
-          target="_blank"
-        >
-          github
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://myreactweathe.netlify.app/" target="_blank">
-          Netlify
-        </a>
-      </p>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,2 +1,81 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
+export default function Weather() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="weather">
+          <form>
+            <input
+              type="search"
+              placeholder="Enter city..."
+              required
+              className="searchButton"
+            />
+            <input type="submit" value="search" className="submitButton" />
+          </form>
+          <h1>New York</h1>
 
+          <div className="row">
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-6">
+                  <img
+                    src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
+                    alt="Cloudy"
+                  />
+                  <span className="degree">17°C</span>
+                </div>
+                <div className="col-5">
+                  <ul className="details">
+                    <li>Precipitation: 21%</li>
+                    <li>Humidity: 92%</li>
+                    <li>Wind: 11 km/h</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <ul className="details-1">
+                <li>
+                  <strong>Weather</strong>
+                </li>
+                <li>Saturday 21:00</li>
+                <li>Cloudy</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/passiedaniels"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pascalia A.
+          </a>
+          , open sourced on{" "}
+          <a
+            href="https://github.com/passiedaniels/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://myreactweathe.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
